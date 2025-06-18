10d7506e4197cdf4eb909eb5683abc65546c01ff
Ajay-Prashad <prashaaj@sheridancollege.ca>
2025-06-17 21:38:53 -0400

    Generating initial Components for Assignment 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {TipHome} from './tip-home/tip-home';
+import {TipResults} from './tip-results/tip-results';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',
+    component: TipHome},
+  {
+    path:'home',
+    component: TipHome
+  },
+  {
+    path:'tip-results',
+    component: TipResults
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Footer} from './footer/footer';
+import {Header} from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [RouterOutlet,Header,Footer],
+  template: `
+    <app-header></app-header>
+<router-outlet></router-outlet>
+    <app-footer></app-footer>
+  `,
   styleUrl: './app.css'
 })
 export class App {
