8e40d5afa103ece1c73995b8fe5bd567b093a125
Ajay-Prashad <prashaaj@sheridancollege.ca>
2025-06-17 22:57:13 -0400

    Router Logic-Copypasted from practice applications,Variable names refactored to match assignment

--- src/app/root-service.ts
+++ src/app/root-service.ts
@@ -4,5 +4,45 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RootService {
+  private formData: any = {};
 
+  setFormData(data: any) {
+    this.formData = data;
+  }
+
+  getFormData() {
+    return this.formData;
+  }
+
+  calculateTip(billAmount: number, serviceQuality: string,
+               roundUp: boolean): { tipAmount: number, total: number }
+
+  {
+
+    let tipPercentage: number;
+
+    switch (serviceQuality) {
+      case 'okay':
+        tipPercentage = 0.15;
+        break;
+      case 'good':
+        tipPercentage = 0.18;
+        break;
+      case 'excellent':
+        tipPercentage = 0.20;
+        break;
+      default:
+        tipPercentage = 0;
+    }
+
+    let tipAmount = billAmount * tipPercentage;
+    if (roundUp) {
+      tipAmount = Math.ceil(tipAmount);
+    }
+
+    const total = billAmount + tipAmount;
+
+    return { tipAmount, total };
+  }
+  constructor() { }
 }
